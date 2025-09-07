ac67d6d5076e0c2a32e41a1773cb7a0918f7546f
Denis Bazhenov <dotsid@gmail.com>
2025-09-07 12:48:32 +0200

    Update settings

--- run.sh
+++ run.sh
@@ -3,28 +3,30 @@
 rm -f ./console
 
 exec qemu-system-aarch64 \
-    `# General settings` \
-    -accel hvf -M virt \
-    -nodefaults -no-user-config -nographic \
+    `# General settings. Using Hypervisor.framework` \
+        -accel hvf -M virt \
+        -nodefaults -no-user-config -nographic \
     `# CPU settings` \
-    -cpu host -smp cpus=1,sockets=1,cores=1,threads=1 \
+        -cpu host -smp cpus=1,sockets=1,cores=1,threads=1 \
     `# Serial port settings` \
-    -device virtio-serial-device \
-    -chardev stdio,signal=off,mux=on,id=virtiocon0 \
-    -device virtconsole,chardev=virtiocon0 \
-    -chardev pty,path=./console,id=pty1 \
-    -device virtconsole,chardev=pty1 \
-    -mon chardev=virtiocon0,mode=readline \
-    `# Disk drive settings` \
-    -drive id=root,file=rootfs.qcow2,format=qcow2,if=none \
-    -device virtio-blk-device,drive=root \
-    `# Network settings` \
-    -device virtio-net-device,netdev=net1 \
-    -netdev user,id=net1 \
-    `# Realtime Clock settings settings. PL031 linux driver is required` \
-    -rtc base=utc,clock=host \
+        -device virtio-serial-device \
+    `# hvc0 serial device with QEMU monitor in a multiplexed mode` \
+        -chardev stdio,signal=off,mux=on,id=console-hvc0 \
+        -device virtconsole,chardev=console-hvc0 \
+        -mon chardev=console-hvc0,mode=readline \
+    `# hvc1 serial device for a ./console pty` \
+        -chardev pty,path=./console,id=console-hvc1 \
+        -device virtconsole,chardev=console-hvc1 \
+    `# Root disk drive` \
+        -drive id=root,file=rootfs.qcow2,format=qcow2,if=none \
+        -device virtio-blk-device,drive=root \
+    `# Network` \
+        -device virtio-net-device,netdev=net1 \
+        -netdev user,id=net1 \
+    `# Realtime Clock. PL031 linux driver is required` \
+        -rtc base=utc,clock=host \
     `# RNG support` \
-    -device virtio-rng-pci \
+        -device virtio-rng-pci \
     `# Linux kernel settings` \
-    -kernel ./Image \
-    -append "console=hvc0 reboot=t root=/dev/vda rw panic=-1"
+        -kernel ./Image \
+        -append "console=hvc0 reboot=t root=/dev/vda rw panic=-1"
