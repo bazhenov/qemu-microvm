42192708689a5d537f8f503f3eb64feeb549c04c
Denis Bazhenov <dotsid@gmail.com>
2026-07-03 14:45:43 +0200

    Delete old code

--- build-initrd.sh
+++ build-initrd.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 #orb sh -c "cd init && cargo build --release --target=aarch64-unknown-linux-musl"
 #cp init/target/aarch64-unknown-linux-musl/release/init ./initrd/init
-cp rootfs2/init rootfs2/sysroot/init
-cp qemu-agent/target/aarch64-unknown-linux-musl/release/server rootfs2/sysroot/bin/server
-(cd rootfs2/sysroot && find . -print0 | cpio --null --create --format=newc) | gzip - > initrd.gz
+cp rootfs/init rootfs/sysroot/init
+cp qemu-agent/target/aarch64-unknown-linux-musl/release/server rootfs/sysroot/bin/server
+(cd rootfs/sysroot && find . -print0 | cpio --null --create --format=newc) | gzip - > initrd.gz
