a794fea293d489cb7779fa9169483e88ee6472fe
Denis Bazhenov <dotsid@gmail.com>
2026-07-16 17:51:21 +0200

    netfilter kernel options to run podman successfully

--- build-initrd.sh
+++ build-initrd.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 #orb sh -c "cd init && cargo build --release --target=aarch64-unknown-linux-musl"
 #cp init/target/aarch64-unknown-linux-musl/release/init ./initrd/init
 cp rootfs/init rootfs/sysroot/init
-cp qemu-agent/target/aarch64-unknown-linux-musl/release/server rootfs/sysroot/bin/server
+cp target/aarch64-unknown-linux-musl/release/server rootfs/sysroot/bin/server
+cargo build --release --target=aarch64-unknown-linux-musl
 (cd rootfs/sysroot && find . -print0 | cpio --null --create --format=newc) | gzip - > initrd.gz
