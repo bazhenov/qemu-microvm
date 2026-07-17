0aee8e0ebdfaef9dd304d89433c39700aa8b0dab
Denis Bazhenov <dotsid@gmail.com>
2026-07-17 16:46:20 +0200

    Fixing ordering issue in build script

--- build-initrd.sh
+++ build-initrd.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 #orb sh -c "cd init && cargo build --release --target=aarch64-unknown-linux-musl"
 #cp init/target/aarch64-unknown-linux-musl/release/init ./initrd/init
 cp rootfs/init rootfs/sysroot/init
-cp target/aarch64-unknown-linux-musl/release/server rootfs/sysroot/bin/server
 cargo build --release --target=aarch64-unknown-linux-musl
+cp target/aarch64-unknown-linux-musl/release/server rootfs/sysroot/bin/server
 (cd rootfs/sysroot && find . -print0 | cpio --null --create --format=newc) | gzip - > initrd.gz
